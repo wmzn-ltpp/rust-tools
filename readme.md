f63f766e9073800323f2b6463113b843dbc9a029
18855190718 <1491579574@qq.com>
2023-12-04 19:48:31 +0800

    更新

--- readme.md
+++ readme.md
@@ -5,9 +5,9 @@
 ## 模块构成
 
 - mod cin
-  - run():String 标准输入
+  - fn run():String 标准输入
 - mod cout
-  - run(&String)
+  - fn run(&String)
 - mod string
-  - to_vec_string(&String): Vec<String> 将字符串转成字符串数组
-  - to_vec_number(&String): Vec<T> 将字符串转成数字 Vec 数组，具体类型为接收返回值的数组类型
+  - fn to_vec_string(&String): Vec<String> 将字符串转成字符串数组
+  - fn to_vec_number<T: std::str::FromStr>(str: &String) -> Vec<T> where T::Err: std::fmt::Debug, 将字符串转成数字 Vec 数组，具体类型为接收返回值的数组类型
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![allow(warnings)]
+use std::error::Error;
+
 mod cin;
 mod cout;
 mod string;
+
 #[derive(Debug)]
 struct User {
     name: String,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    cout::run(&"请输入完成后回车");
     // 测试输入
     let mut str = cin::run();
     cout::run(&"读入数据：");
@@ -17,9 +21,11 @@ fn main() {
     let me: User = User { name: str.clone() };
     cout::run(&me);
     // 测试字符串转String数组
+    str = String::from("a ab abc abcd");
     let res: Vec<String> = string::to_vec_string(&str);
     cout::run(&res);
     // 测试字符串转i32数组
+    str = String::from("1 10 100 1000");
     let res: Vec<i32> = string::to_vec_number(&str);
     cout::run(&res);
     // 测试字符串转i64数组
@@ -29,4 +35,5 @@ fn main() {
     string::update_string_loc_val(&mut str, 0, '1');
     cout::run(&"修改后的读入数据：");
     cout::run(&str);
+    Ok(())
 }
